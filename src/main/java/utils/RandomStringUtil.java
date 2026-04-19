3685ad496755baa5ecb9aab42f42f8d6bc90f431
vinhkhanh <khanhnguyeen2208@gmail.com>
2026-04-19 23:32:17 +0700

    update RandomString (add generateOTP methods)

--- src/main/java/utils/RandomStringUtil.java
+++ src/main/java/utils/RandomStringUtil.java
@@ -4,6 +4,7 @@ import java.security.SecureRandom;
 
 public class RandomStringUtil {
     private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%";
+    private static final String NUMBERS = "0123456789";
 
     public static String generateRandomPassword(int length) {
         SecureRandom random = new SecureRandom();
@@ -11,6 +12,17 @@ public class RandomStringUtil {
         for (int i = 0; i < length; i++) {
             int randomIndex = random.nextInt(CHARACTERS.length());
             sb.append(CHARACTERS.charAt(randomIndex));
+        }
+        return sb.toString();
+    }
+
+    // Thêm hàm tạo mã OTP số
+    public static String generateOTP(int length) {
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(NUMBERS.length());
+            sb.append(NUMBERS.charAt(randomIndex));
         }
         return sb.toString();
     }
